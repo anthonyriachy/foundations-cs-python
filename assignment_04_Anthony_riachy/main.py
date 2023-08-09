3800fc6e81b59ce2659ca89fe7e3089cd61801c3
anthony riachy <anthony.riachy.39@gmail.com>
2023-08-09 15:18:19 +0300

    working with dicitionaries

--- assignment_04_Anthony_riachy/main.py
+++ assignment_04_Anthony_riachy/main.py
@@ -20,53 +20,52 @@ class Stack():
 
 class Graph():
     def __init__(self,size):
-        self.adj_matrix=[]
+        self.adj_matrix={}
 
         self.vertex_list={}
 
-        self.inverted_vertex_list={}
+        
+
+
 
         self.size=size
 
         for i in range(size):
             self.vertex_list[f'name_{i}']=i
-            self.inverted_vertex_list[i]=f'name_{i}'
-            self.adj_matrix.append([0 for i in range(size)]) #create a 2d array and fill it with 0
+            self.adj_matrix[f'name_{i}']={f'name_{i}':0 for i in range(size)}
+            # self.adj_matrix.append([0 for i in range(size)]) #create a 2d array and fill it with 0
             
         
     def print_adj_matrix(self):
         print('adjencent matrix: ')
-        for i in range(self.size):
-            print('at ',i,' is ',self.adj_matrix[i])
+        for i in self.adj_matrix:
+            print('at',i,'is',self.adj_matrix[i])
         print('vertex list: ')
         for i in self.vertex_list:
-            print('name is :',i,' position: ',self.vertex_list[i])
+            print('name is:',i,'position:',self.vertex_list[i])
         print('inverted vertex list:')
-        for i in self.inverted_vertex_list:
-            print('location: ',i,' value :',self.inverted_vertex_list[i])
 
     
     def add_edge(self,name_1,name_2):
-        name1_pos=self.vertex_list[name_1]
-        name2_pos=self.vertex_list[name_2]
-
-        self.adj_matrix[name1_pos][name2_pos]=1
-        self.adj_matrix[name2_pos][name1_pos]=1
+        self.adj_matrix[name_1][name_2]=1
+        self.adj_matrix[name_2][name_1]=1
         
     def remove_edge(self,name_1,name_2):
-        name1_pos=self.vertex_list[name_1]
-        name2_pos=self.vertex_list[name_2]
-        if(self.adj_matrix[name1_pos][name2_pos]==0):
+        if(self.adj_matrix[name_1][name_2]==0):
             print('there is no relation between them')
             return
-
-        self.adj_matrix[name1_pos][name2_pos]=0
-        self.adj_matrix[name2_pos][name1_pos]=0
+        self.adj_matrix[name_1][name_2]=0
+        self.adj_matrix[name_2][name_1]=0
     
 
     def remove_vertex(self,user):
-        user_pos=self.vertex_list[user]
+        del self.adj_matrix[user]
+        self.size=self.size-1
+        for i in self.adj_matrix.values():
+            del i[user]
+        del self.vertex_list[user]
         
+
 
     def check_user(self,user):
         return user in self.vertex_list
@@ -77,28 +76,36 @@ class Graph():
 
         self.size=pos #incrememnt size by 1
         self.vertex_list[user]=pos #add it to the vertex_list
-        self.adj_matrix.append([0 for i in range(self.size)]) # add a new row of zeros
-        self.inverted_vertex_list[self.size]=user
         
+        self.adj_matrix[user]=({i:0 for i in (self.vertex_list)}) # add a new row of zeros
+
+        for i in self.adj_matrix:
+            self.adj_matrix[i][user]=0
+
+
+        
+
     def list_of_friends(self,name):
         friends=[]
         name_pos=self.vertex_list[name]
-        for i in range(self.size):
-            if(self.adj_matrix[name_pos][i]==1):
+        for i in self.adj_matrix[name]:
+            print(self.adj_matrix[name][i])
+            if(self.adj_matrix[name][i]==1):
+
                 friends.append(i) #insert the numebr of the friend 
 
         for i in friends: 
-            print(self.inverted_vertex_list[i])
+            print(i)
             
     def DFS(self,v):
-        visited=[False for i in range(self.size)]
+        visited={i:False for i in self.adj_matrix}        
         stack=Stack()
         stack.push(v)
 
-        while False in visited:
+        while False in visited.values():
 
             if stack.isEmpty():
-                for i in range(len(visited)):
+                for i in visited:
                     if not visited[i]:
                         stack.push(i)
 
@@ -106,20 +113,20 @@ class Graph():
                 v =stack.pop()
                 if not visited[v]:
                     visited[v]=True
-                    print(self.inverted_vertex_list[v],end=" ")
-                    for i in range(len(self.adj_matrix[v])):
+                    print(v,end=" ")
+                    for i in self.adj_matrix[v]:
                         if(self.adj_matrix[v][i]==1 and not visited[i]):
                             stack.push(i)
-        
+        print('\n')
 def main():
-    people=Graph(10)
+    people=Graph(8)
 
-    people.add_edge('name_2',"name_3")
-    people.add_edge('name_2',"name_4")
-    people.add_edge('name_2',"name_6")
-    people.add_edge('name_2',"name_9")
-    people.add_edge('name_2',"name_0")
-    menu="1.Add a user to the platform.\n2. Remove a user from the platform\n3.Send a friend request to another user.\n4.Remove a friend from your list\n5.View your list of friends.\n6.View the list of users of the platform.\n7.Exit\n- - - - - - - - - - - - - - - -\nEnter a choice:"
+    # people.add_edge('name_2',"name_3")
+    # people.add_edge('name_2',"name_4")
+    # people.add_edge('name_2',"name_6")
+    # people.add_edge('name_2',"name_9")
+    # people.add_edge('name_2',"name_0")
+    menu="1.Add a user to the platform.\n2.Remove a user from the platform\n3.Send a friend request to another user.\n4.Remove a friend from your list\n5.View your list of friends.\n6.View the list of users of the platform.\n7.Exit\n- - - - - - - - - - - - - - - -\nEnter a choice:"
     choice=0
 
     while(choice!=7):
@@ -153,7 +160,7 @@ def main():
                 if(not user_exists):
                     print('userName does not exist')
                 else:
-                    people.remove_vertex(user)
+                    people.add_edge(user_1,user_2)
         if(choice==4):
             user_1=input("enter the name of the first user")
             user_2=input("enter the name of the second user") #check if users are here
@@ -162,7 +169,7 @@ def main():
             user=input('enter the user you want to find his friends')
             people.list_of_friends(user)
         if(choice==6):
-            people.DFS(0)
+            people.DFS('name_0')
 
 
 
